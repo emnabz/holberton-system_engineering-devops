04bbba82858a6e4b16bd2dee5f9f98faee4a6345
emna <1947@holbertonschool.com>
2021-05-03 23:56:49 +0100

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 import requests
 import sys
-
-"""a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress
+"""
 
 
 if __name__ == '__main__':
 
-    employee_id = sys.argv[1]
+    id_c = sys.argv[1]
     task_title = []
     complete = 0
     total_task = 0
-    url_user = "https://jsonplaceholder.typicode.com/users/" + employee_id
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
     res = requests.get(url_user).json()
     name = res.get('name')
     url_task = "https://jsonplaceholder.typicode.com/todos/"
     res_task = requests.get(url_task).json()
     for i in res_task:
-        if i.get('userId') == int(employee_id):
+        if i.get('userId') == int(id_c):
             if i.get('completed') is True:
                 task_title.append(i['title'])
                 complete += 1
